cdd2bcbc287719651761c84c337992f9636facdd
wuming5 <910388642@qq.com>
2018-03-05 11:45:52 +0800

    各页基本结构完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -2,20 +2,20 @@
  * Created by Administrator on 2018/3/4.
  */
 $(function(){
-    var currentPage = 1;
-    var pageSize = 5;
-    function render(){
-        $.ajax({
-            type:'GET',
-            ulr:'/user/queryUser',
-            data:{
-                page:currentPage,
-                pageSize:pageSize
-            },
-            success:function(info){
-                console.log(info);
-            }
-        })
-    }
+    //var currentPage = 1;
+    //var pageSize = 5;
+    //function render(){
+    //    $.ajax({
+    //        type:'GET',
+    //        ulr:'/user/queryUser',
+    //        data:{
+    //            page:currentPage,
+    //            pageSize:pageSize
+    //        },
+    //        success:function(info){
+    //            console.log(info);
+    //        }
+    //    })
+    //}
 
 });
